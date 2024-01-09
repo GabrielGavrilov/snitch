a1ae485b5c8a384f1f0a761cefd24430d8d1eec9
Gabriel Gavrilov <gabrielgavrilov11@gmail.com>
2024-01-09 06:54:47 -0600

    update

--- todo.cpp
+++ todo.cpp
@@ -10,23 +10,31 @@ std::vector<std::string> todo::generate_todo_list_from_file(std::string path)
 
 	if(!file.is_open())
 	{
-		std::cout << "[Snitch]: There has been an issue." << std::endl;
+		std::cout << "[Snitch]: File does not exist." << std::endl;
 	}
 
 	while(std::getline(file, line))
 	{
-		todo_list.push_back(get_todo_item(line));
+		std::cout << line << std::endl;
+		todo_list.push_back(todo::check_for_comment(line));
 	}
 
 	return todo_list;
 }
 
-std::string todo::get_todo_item(std::string text)
+std::string todo::check_for_comment(std::string text)
 {
 	if(text.find("//") != std::string::npos)
 	{
-		return text;
+		todo::check_for_todo_item(text);
 	}
+}
 
-	return "";
+std::string todo::check_for_todo_item(std::string text)
+{
+	if(text.find("TODO") != std::string::npos)
+	{
+		std::string todo_item = text.substr(text.find("TODO:")+6, text.length());
+		return todo_item;
+	}
 }
--- todo.h
+++ todo.h
@@ -9,5 +9,6 @@ class todo
 public:
 	static std::vector<std::string> generate_todo_list_from_file(std::string path);
 private: 
-	static std::string get_todo_item(std::string text);
+	static std::string check_for_comment(std::string text);
+	static std::string check_for_todo_item(std::string text);
 };
